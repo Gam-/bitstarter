05c546242bd66464f670307a91594dcdc72f4c47
Garrett Meyer <1570601@lfschools.net>
2014-09-25 08:20:25 -0500

    Create web.js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs =  require('fs');
+var app = express.createServer(express.logger());
+
+var index = fs.readFileSync("index.html");
+var buffer = new Buffer(index);
+
+app.get('/', function(request, response) {
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
