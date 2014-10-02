12559ac64454c43c2a19bfb4f93ca93f58ce95b2
Gam- <1570601@lfschools.net>
2014-10-02 13:23:12 +0000

    new stuff

--- web.js
+++ web.js
@@ -5,11 +5,13 @@ var app = express.createServer(express.logger());
 var index = fs.readFileSync("index.html");
 var buffer = new Buffer(index);
 
-app.get('/', function(request, response) {
+app.get('/', function(request, response)
+{
   response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
+app.listen(port, function()
+{
   console.log("Listening on " + port);
 });
